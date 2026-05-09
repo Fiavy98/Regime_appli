1f912089a90184810ba7052feeb4d7d72ab7a465
Tsinjo99 <tsinjomaintso@gmail.com>
2026-05-09 23:36:32 +0300

    value = admin

--- app/Views/login.php
+++ app/Views/login.php
@@ -34,7 +34,7 @@
         <form method="post" action="<?= base_url('/login') ?>" class="login-form">
           <div class="field">
             <label class="form-label">Adresse email</label>
-            <input class="input" type="email" name="email" value="andry@gmail.com" required>
+            <input class="input" type="email" name="email" value="admin@gmail.com" required>
           </div>
           <div class="field">
             <label class="form-label">Mot de passe</label>
